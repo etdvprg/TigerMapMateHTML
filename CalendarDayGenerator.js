31ecd27532b10da1fd7ee83fca0258d74e72d230
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-05-10 19:44:32 +0800

    STARTED: current day and next day schedule

--- CalendarDayGenerator.js
+++ CalendarDayGenerator.js
@@ -1,6 +1,17 @@
-function generateDays(startingDay, endingDay, lastMonthEnd) {
+function generateCalendar(startingDay, endingDay, lastMonthEnd) {
     const div = document.getElementById("schedule-calendar");
 
+    // Generates the heading days
+    const DAYS_OF_WEEK = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
+    for(var i=0; i<7; i++) {
+        var dayOfWeek = document.createElement("div");
+        dayOfWeek.className = "calendar-day-of-week";
+        dayOfWeek.textContent = DAYS_OF_WEEK[i];
+        div.appendChild(dayOfWeek);
+    }
+
+
+    // Generates the days
     var dayCount = lastMonthEnd;
     for (var i=1; i<36; i++) {
         var day = document.createElement("div");
@@ -23,20 +34,3 @@ function generateDays(startingDay, endingDay, lastMonthEnd) {
     }
     
 }
-
-// function generateWeekdays() {
-//     const div = document.getElementById("schedule-calendar");
-//     for (var i=0; i<7; i++) {
-//         var day = document.createElement("div");
-//         if (i == startingDay) { /*May starts on monday so 2nd block yung 1 */
-//             dayCount = 1;
-//         }
-//         if (i <= endingDay) {
-//             day.textContent = dayCount;
-//         } 
-        
-//         div.appendChild(day);
-//         dayCount++;
-//     }
-    
-// }
