36bb859eab9683395e5590c8fb38918a9a2ad896
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-05-12 14:42:19 +0800

    Copied all the files into their respective src folders

---
+++ src/js/ObjectFilter.js
@@ -0,0 +1,49 @@
+// Code Retrieved from here: https://www.youtube.com/watch?v=HRpjAGT7X4Y
+// Thank you Benjamin Flanders
+
+function displayAll() {
+    filterObject('all', "feedbox-container");
+    filterObject('all', "feedbox-mini-container");
+    filterObject('all', "schedule-item");
+}
+
+
+function filterObject (c, type) {
+    var x = document.getElementsByClassName(type);
+    
+    if (c == "all") c = "";
+    for (var i=0; i < x.length; i++) {
+        removeClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) {
+            addClass(x[i], "show");
+        }
+    }
+}
+
+function addClass(element, /*class*/ name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for(i = 0; i<arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+function removeClass(element, name) {
+    var i, arr1, arr2;  
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for(i = 0; i<arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i], 1));
+        }
+    }
+    element.className = arr1.join(" ");
+}
+
+function hideClass(className) {
+    var x = document.getElementsByClassName(className);
+    removeClass(x[0], className);
+}
---
+++ src/js/navBtnResponsive.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.querySelector('.navbar-button');
+    const container = document.querySelector('.nav-dropdown-container');
+  
+    button.addEventListener('click', function() {
+        container.classList.toggle('visible');
+    });
+});
+
+  
